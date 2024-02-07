de2e0807bbb9f3681a472afad0e4ee7c967ca0ea
Matheus Felipe Gremes <matheusfelipegremes@gmail.com>
2024-02-07 08:23:00 -0300

    Refactor dataset loading logic to handle triplets
    - Modify the `LabeledFacesWildDataset` class to handle triplets of images instead of pairs.
    - Update the initialization method to read the annotation file properly and organize the data into triplets.
    - Remove unnecessary attributes and methods related to handling matched and mismatched pairs.
    - Update the `__getitem__` method to retrieve images based on the triplet format.
    - Update the example in the docstring to reflect the new format of the annotations file.

--- src/facial_recognizer/dataloaders.py
+++ src/facial_recognizer/dataloaders.py
@@ -21,7 +21,7 @@ Usage:
     ```
     from facial_recognizer.dataset import LabeledFacesWildDataset
 
-    annotations_file = 'dataset/pairsDevTrain.txt'
+    annotations_file = 'dataset/lfw_funneled/pairs_01.txt'
     img_dir = 'dataset/lfw_funneled'
 
     dataset = LabeledFacesWildDataset(img_dir, annotations_file)
@@ -60,8 +60,7 @@ class LabeledFacesWildDataset(Dataset):
 
     Attributes
     ----------
-        df_match (DataFrame): DataFrame containing information about matched pairs of images.
-        df_miss_match (DataFrame): DataFrame containing information about mismatched pairs of images.
+        df_tripled (DataFrame): DataFrame containing information about triplets of images.
         img_dir (str): Path to the directory containing the images.
         transform (callable): A function/transform to apply to the images.
 
@@ -72,11 +71,26 @@ class LabeledFacesWildDataset(Dataset):
 
     Examples
     --------
-        dataset = LabeledFacesWildDataset(img_dir='lfw_funneled',
-                                          annotations_file='pairsDevTrain.txt',
-                                          transform=transforms.Compose([transforms.Resize(256),
-                                                                        transforms.CenterCrop(224),
-                                                                        transforms.ToTensor()]))
+        # Example of how the data is formatted in the annotations_file:
+        # Each line contains paths to four images, representing a triplet
+        # The first two images are matches, and the last two images are mismatches
+        # Example:
+
+        # Abdel_Nasser_Assidi/Abdel_Nasser_Assidi_0001.jpg
+        # Abdel_Nasser_Assidi/Abdel_Nasser_Assidi_0002.jpg
+        # Aaron_Tippin/Aaron_Tippin_0001.jpg
+        # Enos_Slaughter/Enos_Slaughter_0001.jpg
+        #
+        # Ai_Sugiyama/Ai_Sugiyama_0001.jpg
+        # Ai_Sugiyama/Ai_Sugiyama_0002.jpg
+        # Aaron_Tippin/Aaron_Tippin_0001.jpg
+        # Juan_Carlos_Ortega/Juan_Carlos_Ortega_0001.jpg
+        #
+        # Ai_Sugiyama/Ai_Sugiyama_0001.jpg
+        # Ai_Sugiyama/Ai_Sugiyama_0004.jpg
+        # Aaron_Tippin/Aaron_Tippin_0001.jpg
+        # Marlon_Devonish/Marlon_Devonish_0001.jpg
+
     """
 
     def __init__(self, img_dir: str, annotations_file: str, transform: None = None):
@@ -90,11 +104,11 @@ class LabeledFacesWildDataset(Dataset):
                                              Default is None.
         """
         with Path(annotations_file).open() as file:
-            num_lines = int(next(file))
+            data = [line.strip() for line in file if line.strip()]
 
-        self.df_match = pd.read_csv(annotations_file, sep='\t', skiprows=1, nrows=num_lines, header=None)
-        self.df_miss_match = pd.read_csv(annotations_file, sep='\t', skiprows=num_lines + 1, header=None)
+        data_split = [data[i : i + 4] for i in range(0, len(data), 4)]
 
+        self.df_tripled = pd.DataFrame(data_split)
         self.img_dir = img_dir
         self.transform = transform
 
@@ -124,72 +138,23 @@ class LabeledFacesWildDataset(Dataset):
         - The first two images represent a match.
         - The last two images represent a mismatch.
         """
-        match_images = self._get_match_images(idx)
-        mismatch_images = self._get_mismatch_images(idx)
-        return match_images[0], match_images[1], mismatch_images[0], mismatch_images[0]
+        match_1_img_path = Path(self.img_dir) / Path(self.df_tripled.iloc[idx, 0])
+        match_2_img_path = Path(self.img_dir) / Path(self.df_tripled.iloc[idx, 1])
+        mismatch_1_img_path = Path(self.img_dir) / Path(self.df_tripled.iloc[idx, 2])
+        mismatch_2_img_path = Path(self.img_dir) / Path(self.df_tripled.iloc[idx, 3])
 
-    def _get_match_images(self, idx: int) -> tuple[Tensor, Tensor]:
-        """
-        Retrieve a pair of images representing a match at the given index.
-
-        Args:
-            idx (int): Index of the match pair to retrieve.
-
-        Returns
-        -------
-            tuple: A tuple containing two images representing a match.
-
-        This method retrieves a pair of images representing a match from the dataset at the given index.
-        It returns a tuple containing two images. The first image corresponds to the first image in the match pair,
-        and the second image corresponds to the second image in the match pair.
-        """
-        match_1_name = self.df_match.iloc[idx, 0]
-        match_1_img_number = self.df_match.iloc[idx, 1]
-        match_2_img_number = self.df_match.iloc[idx, 2]
-
-        match_1_img_path = Path(self.img_dir) / match_1_name / f'{match_1_name}_{match_1_img_number:04d}.jpg'
-        match_2_img_path = Path(self.img_dir) / match_1_name / f'{match_1_name}_{match_2_img_number:04d}.jpg'
-
-        image_1 = read_image(str(match_1_img_path))
-        image_2 = read_image(str(match_2_img_path))
+        match_image_1 = read_image(str(match_1_img_path))
+        match_image_2 = read_image(str(match_2_img_path))
+        mismatch_image_1 = read_image(str(mismatch_1_img_path))
+        mismatch_image_2 = read_image(str(mismatch_2_img_path))
 
         if self.transform:
-            image_1 = self.transform(image_1)
-            image_2 = self.transform(image_2)
+            match_image_1 = self.transform(match_image_1)
+            match_image_2 = self.transform(match_image_2)
+            mismatch_image_1 = self.transform(mismatch_image_1)
+            mismatch_image_2 = self.transform(mismatch_image_2)
 
-        return image_1, image_2
-
-    def _get_mismatch_images(self, idx: int) -> tuple[Tensor, Tensor]:
-        """
-        Retrieve a pair of images representing a mismatch at the given index.
-
-        Args:
-            idx (int): Index of the mismatch pair to retrieve.
-
-        Returns
-        -------
-            tuple: A tuple containing two images representing a mismatch.
-
-        This method retrieves a pair of images representing a mismatch from the dataset at the given index.
-        It returns a tuple containing two images. The first image corresponds to one image in the mismatch pair,
-        and the second image corresponds to the other image in the mismatch pair.
-        """
-        mismatch_1_name = self.df_miss_match.iloc[idx, 0]
-        mismatch_2_name = self.df_miss_match.iloc[idx, 2]
-        mismatch_1_img_number = self.df_miss_match.iloc[idx, 1]
-        mismatch_2_img_number = self.df_miss_match.iloc[idx, 3]
-
-        mismatch_1_img_path = Path(self.img_dir) / mismatch_1_name / f'{mismatch_1_name}_{mismatch_1_img_number:04d}.jpg'
-        mismatch_2_img_path = Path(self.img_dir) / mismatch_2_name / f'{mismatch_2_name}_{mismatch_2_img_number:04d}.jpg'
-
-        image_1 = read_image(str(mismatch_1_img_path))
-        image_2 = read_image(str(mismatch_2_img_path))
-
-        if self.transform:
-            image_1 = self.transform(image_1)
-            image_2 = self.transform(image_2)
-
-        return image_1, image_2
+        return match_image_1, match_image_2, mismatch_image_1, mismatch_image_2
 
 
 def main() -> int:
@@ -200,10 +165,10 @@ def main() -> int:
     ----
     This function is intended for testing purposes only.
     """
-    annotations_file = r'dataset\pairsDevTrain.txt'
     img_dir = r'dataset\lfw_funneled'
+    pairs_txt = r'dataset\lfw_funneled\pairs_01.txt'
 
-    dataset = LabeledFacesWildDataset(img_dir, annotations_file)
+    dataset = LabeledFacesWildDataset(img_dir, pairs_txt)
 
     image_1, image_2, image_3, image_4 = dataset[0]
 
